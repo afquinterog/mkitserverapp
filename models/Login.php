13c83cc627d9b9048a2c2e524a41621747352050
Andres Felipe Quintero Garcia <rocoutp@gmail.com>
2016-01-19 16:27:07 -0500

    Fix error on login with wrong data

--- models/Login.php
+++ models/Login.php
@@ -57,7 +57,9 @@ class Login extends Model
     public function login()
     {
         if ($this->validate()) {
-            return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600*24*30 : 0);
+            if($this->getUser() != null){
+                return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600*24*30 : 0);
+            }
         }
         return false;
     }
